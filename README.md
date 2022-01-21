25809faff6077903b1d9ea2602553b81cea87a4d
jonnn13 <jon.s.ayuso@gmail.com>
2022-01-21 10:42:37 +0100

    nuevas funciones

--- README.md
+++ README.md
@@ -2,14 +2,12 @@
 
 en este fichero se explica el contenido de nuestro repositorio
 
-otro cambio
-
-otoro cambio
-
-cambio 5
 
 # Contenido
 
 ## Exportar csv
 
 hemos exportado en csv en el script llamado *script_exportar.r*
+
+
+Explicación sessioninfo, order y aggegator
--- script_exportar.R
+++ script_exportar.R
@@ -3,3 +3,24 @@ library(MLDataR)
 tir = MLDataR::thyroid_disease
 
 write.csv(tir, file = "dataset_tiroides.csv")
+
+
+# Inofrmacion de la sesion 
+sessionInfo()
+
+
+# order --> es para ordenar (como sort pero sort para vectores, order para bases de datos )
+head(tir)
+
+sort(tir$patient_age)
+
+tir2= tir[order(tir$patient_age),]
+
+tir_completo = tir[complete.cases(tir),] # complete.cases--> para quitar todos los pacientes que tengan informacion misses
+
+# aggregate --> paar agurpar por grupos y hacer calculos estadísticos 
+edad_tyr = aggregate(tir_completo$patient_age, list(tir_completo$ThryroidClass), mean)
+
+names(edad_tyr)= c("Tiroides", "Media")
+
+aggregate(tir_completo$patient_age, list(tir_completo$ThryroidClass, tir_completo$patient_gender), mean)
